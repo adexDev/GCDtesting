104e7bd58de3efa106855762c3176e3a59f20021
adexDev <adexDeveloper@gmail.com>
2013-04-08 11:45:42 +0100

    Initial Commit

---
+++ GCDtesting/GTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GTAppDelegate.h
+//  GCDtesting
+//
+//  Created by User on 08/04/2013.
+//  Copyright (c) 2013 amaCoder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GCDtesting/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GCDtesting
+//
+//  Created by User on 08/04/2013.
+//  Copyright (c) 2013 amaCoder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GTAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GTAppDelegate class]));
+    }
+}
